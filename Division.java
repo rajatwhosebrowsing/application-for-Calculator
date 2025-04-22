838cb990f2db065e74f7389d349689ecb953116c
Rajat Dwivedi <dwiv9451@gmail.com>
2025-04-22 23:02:59 +0530

    Update Division.java

--- Division.java
+++ Division.java
@@ -2,7 +2,7 @@ public class Division {
     public static double divide(double a, double b) {
         try {
             if (b == 0) {
-                throw new ArithmeticException("Cannot divide by zero.");
+                throw new ArithmeticException("Cannot divide by zero."); 
             }
             return a / b;
         } catch (ArithmeticException e) {
